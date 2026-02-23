7237ed154764d0984cda8eb85784dc71f199e359
DanikINV <Daniil16200@yandex.ru>
2026-02-23 22:01:22 +0300

    Home work

---
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContentView(R.layout.activity_main)
+
+        val rootView = findViewById<View>(R.id.root_layout)
+
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+            insets
+        }
+
+        val searchButton = findViewById<MaterialButton>(R.id.btn_search)
+        val mediaLibraryButton = findViewById<MaterialButton>(R.id.btn_media)
+        val settingsButton = findViewById<MaterialButton>(R.id.btn_settings)
+
+        searchButton.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@MainActivity,
+                    SearchActivity::class.java
+                )
+            )
+        }
+
+        mediaLibraryButton.setOnClickListener {
+            startActivity(Intent(this, MediaLibraryActivity::class.java))
+        }
+
+        settingsButton.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+    }
+}
